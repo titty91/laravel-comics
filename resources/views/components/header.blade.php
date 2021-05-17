45608084686f3589527a3e861815edb14b48ecd0
titty91 <tznbaraldi@gmail.com>
2021-05-17 17:09:44 +0200

    nw file

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,2 @@
+<!-- in qst pagina tutto qll che scrivo viene importato in 'main-layout-blade.php' nella riga @include('components.header') -->
+<h1>ciao Alter</h1>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main-layout')
+@section('content')
+<h1>Hello World</h1>
+@endsection
