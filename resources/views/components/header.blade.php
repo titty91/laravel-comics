7a93f59c35801c79eab2dca89a121ea8b5e0e3b9
titty91 <tznbaraldi@gmail.com>
2021-05-18 15:43:54 +0200

    mod file css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,2 +1,16 @@
-<!-- in qst pagina tutto qll che scrivo viene importato in 'main-layout-blade.php' nella riga @include('components.header') -->
-<h1>ciao Alter</h1>
+{{-- in qst pagina tutto qll che scrivo viene importato in 'main-layout-blade.php' nella riga @include('components.header')  --}}
+<div class="center">
+
+    <ul>
+        <li>
+            Characters
+        </li>
+        <li>
+            Comics
+        </li>
+        <li>
+            Movies
+        </li>
+    </ul>
+    
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,3 @@
 @extends('layouts.main-layout')
 @section('content')
-<h1>Hello World</h1>
 @endsection
